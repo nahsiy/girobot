14402e57ff049a012b3ec1001a4558c5b8acc913
yishan <yishan@protonmail.com>
2022-01-28 21:29:36 +0100

    Correction Syntaxique

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-############################### Bot discord de Giroll ###############################
-##### Import des librairies #####
+"""Bot discord de Giroll"""
 
+# Import des librairies
 from distutils.log import error
 from email import message
 from lib2to3.pgen2.token import ASYNC
@@ -14,36 +14,37 @@ import sqlite3
 from pyparsing import Word
 from dotenv import load_dotenv
 
-##### Misen en place du Prefixe #####
+# Mise en place du Prefixe
 bot = commands.Bot(command_prefix='!')
 
 load_dotenv(dotenv_path="config")
 
 
-
-##### Initialisation du Bot #####
+# Initialisation du Bot
 @bot.event
 async def on_ready():
     game = discord.Game("with the API")
     await bot.change_presence(status=discord.Status.online, activity=game)
-    print ("Connected to discord")
+    print("Connected to discord")
 
-##### Ping #####
+
+# Ping
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
-    print ("ping envoyé")
+    print("ping envoyé")
 
 
-#################### <QUOTES> ####################
-##### Verifier que la base de données est créé et lancé #####
+"""<QUOTES>"""
+# Verifier que la base de données est créée et lancée
 db = sqlite3.connect('/home/yishan/girobot/quotes.db')
 cursor = db.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS quotes(hash TEXT primary key, user TEXT, message TEXT, date_added TEXT)')
 print("Loaded database")
 db.commit()
 
-##### Menu Help #####
+
+# Menu Help
 @bot.command()
 async def quotehelp(ctx):
     embed = discord.Embed(name="help")
@@ -54,40 +55,39 @@ async def quotehelp(ctx):
     await ctx.send(embed=embed)
 
 
-##### Effectuer une quote #####
+# Effectuer une quote
 @bot.command()
-async def quote(ctx,*, message: str):
-
-    #split the message into words
+async def quote(ctx, *, message: str):
+    # split the message into words
     string = str(message)
     temp = string.split()
 
-    #take the username out
+    # take the username out
     user = temp[0]
     del temp[0]
 
-    #join the message back together
+    # join the message back together
     text = " ".join(temp)
-    
-    if user[1]!='@':
+
+    if user[1] != '@':
         await ctx.send("Utilise ```@[user] [message]``` pour quoter un utilisateur")
         return
 
-    uniqueID = hash(user+message)
+    unique_id = hash(user + message)
 
     # date et heure de la quote :
     time = datetime.datetime.now()
     formatted_time = str(time.strftime("%d-%m-%Y %H:%M"))
 
     # Cherche si la quote est déjà dans la BDD 
-    cursor.execute("SELECT count(*) FROM quotes WHERE hash = ?",(uniqueID,))
+    cursor.execute("SELECT count(*) FROM quotes WHERE hash = ?", (unique_id,))
     find = cursor.fetchone()[0]
 
-    if find>0:
+    if find > 0:
         return
 
     # Sinon on l'ajoute
-    cursor.execute("INSERT INTO quotes VALUES(?,?,?,?)",(uniqueID,user,text,formatted_time))
+    cursor.execute("INSERT INTO quotes VALUES(?,?,?,?)", (unique_id, user, text, formatted_time))
     await ctx.send("Quote ajoutée avec succès")
 
     db.commit()
@@ -95,44 +95,45 @@ async def quote(ctx,*, message: str):
     # Nombre de quotes dans la BDD
     rows = cursor.execute("SELECT * from quotes")
 
-    #log
-    print(str(len(rows.fetchall()))+". added - "+str(user)+": \""+str(text)+"\" to database at "+formatted_time)
+    # log
+    print(str(len(rows.fetchall())) + ". added - " + str(user) + ": \"" + str(
+        text) + "\" to database at " + formatted_time)
 
-##### Afficher une quote aléatoirement #####
+
+# Afficher une quote aléatoirement
 @bot.command()
 async def random(ctx):
-
     cursor.execute("SELECT user,message,date_added FROM quotes ORDER BY RANDOM() LIMIT 1")
     query = cursor.fetchone()
 
-    #log
-    print(query[0]+": \""+query[1]+"\" printed to the screen "+str(query[2]))
+    # log
+    print(query[0] + ": \"" + query[1] + "\" printed to the screen " + str(query[2]))
 
     # Sortie en embed
-    style = discord.Embed(name="responding quote", description="- "+str(query[0])+" "+str(query[2]))
+    style = discord.Embed(name="responding quote", description="- " + str(query[0]) + " " + str(query[2]))
     style.set_author(name=str(query[1]))
     await ctx.send(embed=style)
 
-##### Afficher une quote d'un utilisateur précis aléatoirement #####
+
+# Afficher une quote d'un utilisateur précis aléatoirement #####
 @bot.command()
 async def getquote(ctx, message: str):
-    
-    #sanitise name
+    # sanitise name
     user = (message,)
 
     try:
-        #query random quote from user
-        cursor.execute("SELECT message,date_added FROM quotes WHERE user=(?) ORDER BY RANDOM() LIMIT 1",user)
+        # query random quote from user
+        cursor.execute("SELECT message,date_added FROM quotes WHERE user=(?) ORDER BY RANDOM() LIMIT 1", user)
         query = cursor.fetchone()
 
-        #adds quotes to message
-        output = "\""+str(query[0])+"\""
+        # adds quotes to message
+        output = "\"" + str(query[0]) + "\""
 
-        #log
-        print(message+": \""+output+"\" printed to the screen "+str(query[1]))
+        # log
+        print(message + ": \"" + output + "\" printed to the screen " + str(query[1]))
 
         # Embed
-        style = discord.Embed(name="responding quote", description="- "+message+" "+str(query[1]))
+        style = discord.Embed(name="responding quote", description="- " + message + " " + str(query[1]))
         style.set_author(name=output)
         await ctx.send(embed=style)
 
@@ -142,60 +143,64 @@ async def getquote(ctx, message: str):
 
     db.commit()
 
-####################  </QUOTES> ####################
 
-####################  <NOSTALGIE BOT IRC> ####################
-#### Barman ####
+'''</QUOTES>'''
 
-# Kawa
+'''<NOSTALGIE BOT IRC>'''
+
+
+# Barman
+
+
 @bot.command()
-async def café(ctx,member: discord.Member):
+async def cafe(ctx, member: discord.Member):
     user = member.mention
     await ctx.send(f"{ctx.author.display_name} offre un café à {user}")
 
-@café.error
-async def café_error(ctx, error):
+
+@cafe.error
+async def cafe_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(f"Girobot offre un café à {ctx.author.mention}")
 
-# Thé
 
 @bot.command()
-async def thé(ctx,member: discord.Member):
+async def the(ctx, member: discord.Member):
     user = member.mention
     await ctx.send(f"{ctx.author.display_name} offre un thé à {user}")
 
-@thé.error
-async def thé_error(ctx, error):
+
+@the.error
+async def the_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(f"Girobot offre un thé à {ctx.author.mention}")
 
-# Bière
 
 @bot.command()
-async def bière(ctx,member: discord.Member):
+async def biere(ctx, member: discord.Member):
     user = member.mention
     await ctx.send(f"{ctx.author.display_name} offre une bière à {user}")
 
-@bière.error
-async def bière_error(ctx, error):
+
+@biere.error
+async def biere_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(f"Girobot offre une bière à {ctx.author.mention}")
 
-# #### Les mots interdits ####
-# @bot.event
-# async def on_message(message):
-#     if message.author.id == bot.user.id:
-#         return
-#     msg_content = message.content.lower()
-#     words_forbidden= ['lol','mdr','ptdr', 'windows']
-#     if any(word in msg_content for word in words_forbidden):
-#         await message.delete()
-#         await message.channel.send(f"{message.author.mention}, attention à ton langage !")
+
+# Les mots interdits
+@bot.event
+async def on_message(message):
+    if message.author.id == bot.user.id:
+        return
+    msg_content = message.content.lower()
+    words_forbidden = ['lol', 'mdr', 'ptdr', 'windows']
+    if any(word in msg_content for word in words_forbidden):
+        await message.delete()
+        await message.channel.send(f"{message.author.mention}, attention à ton langage !")
 
 
-####################  </NOSTALGIE BOT IRC> ###################
+'''</NOSTALGIE BOT IRC>'''
 
-
-##### TOKEN #####
+'''TOKEN'''
 bot.run(os.getenv("TOKEN"))
